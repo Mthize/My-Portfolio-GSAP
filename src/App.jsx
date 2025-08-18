37a18bc96439fcf28f0fd0e49f1f24360d56bd2b
Thapelo Mthize <122882874+Mthize@users.noreply.github.com>
2025-08-19 00:17:06 +0200

    Project Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Hero from "./sections/Hero";
-import About from "./sections/About";              
-
+import About from "./sections/About";
+import TechStack from "./sections/TechStack";
+import Project from "./sections/Project";
 
 const App = () => {
   return (
     <div>
       <Hero />
       <About />
+      <TechStack />
+      <Project />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
